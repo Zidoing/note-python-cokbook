4e047b1ab0de445aca8287621e8469fbf93857bf
周磊 <zhoulei_8888@sina.com>
2018-11-06 11:24:24 +0800

    init

---
+++ 1.1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Z Lei on 06/11/2018.
+
+
+p = (4, 5)
+x, y = p
+print(x, y)
+
+data = ["ACME", 50, 91.1, (2012, 12, 21)]
+
+name, shares, price, date = data
+
+print(name)
+
+print(date)
+
+name, shares, price, (year, mon, day) = data
+
+print(name)
+print(year)
+print(mon)
+print(day)
+
+p = (4, 5)
+# x,y,z = p
